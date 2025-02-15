699d986cbffb506fe8286e88eb51723b6e513de4
Preman Das <dasman809@gmail.com>
2025-02-16 01:40:38 +0800

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Geri-Das, Preman on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
